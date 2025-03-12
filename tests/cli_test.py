be26fb28ae320140781d37f6889338b5efba7f61
Vinko Kolobara <kolobara.vinko@gmail.com>
2025-03-12 09:31:57 +0100

    test: add basic cli tests

---
+++ tests/cli_test.py
@@ -0,0 +1,54 @@
+import os
+import pathlib
+
+import pytest
+from typer.testing import CliRunner
+
+from agentic_radar.cli import app
+
+runner = CliRunner()
+
+
+def test_help():
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
+
+
+BASE_DIR_LANGGRAPH = "examples/langgraph"
+BASE_DIR_CREWAI = "examples/crewai"
+
+
+@pytest.fixture()
+def params(request, tmp_path: pathlib.Path):
+    tmp_path.mkdir(exist_ok=True)
+    return (request.param[0], str(tmp_path / request.param[1]))
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        (os.path.join(BASE_DIR_LANGGRAPH, dir), "report.html")
+        for dir in os.listdir(BASE_DIR_LANGGRAPH)
+    ],
+    indirect=True,
+)
+def test_langgraph(params):
+    i, o = params
+    result = runner.invoke(app, ["-i", i, "-o", o, "langgraph"])
+    assert result.exit_code == 0
+    assert o in result.stdout
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        (os.path.join(BASE_DIR_CREWAI, dir), "report.html")
+        for dir in os.listdir(BASE_DIR_CREWAI)
+    ],
+    indirect=True,
+)
+def test_crewai(params):
+    i, o = params
+    result = runner.invoke(app, ["-i", i, "-o", o, "crewai"])
+    assert result.exit_code == 0
+    assert o in result.stdout
